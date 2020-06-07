80818ea6d177dda5e7487a3cbe90231932b38fc2
bloodnighttw <bbeenn1227@gmail.com>
2020-06-07 21:25:01 +0800

    Initial commit

---
+++ Lmcps/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LmcpsConfig(AppConfig):
+    name = 'Lmcps'
---
+++ Lmcps/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def redirect_to_index(request):
+    return redirect('/index/')
+
+
